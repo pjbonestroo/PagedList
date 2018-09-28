02648d4b7fc10cda6480970dfb059f044895a465
pieterjanbonestroo <pbonestroo@vmi-group.com>
2018-09-28 11:00:54 +0200

    add minification to webpack config

--- python/__target__/org.transcrypt.__runtime__.js
+++ python/__target__/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2018-09-28 10:43:23
+// Transcrypt'ed from Python, 2018-09-28 11:00:17
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,6 @@ module.exports = {
     mode: 'production',
     devtool: 'source-map',
     optimization: {
-      minimize: false
+      minimize: true,
     },
   };
